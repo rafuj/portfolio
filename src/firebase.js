e9c21155863f4711c2d59ecafada6b59480a4c9c
rafuj <raihanrafuj0078@gmail.com>
2021-12-22 00:55:43 +0600

    Firebase Database Upgraded

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 
 const app = initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyCx5oU7uKp0LgstOb9z9T-Jdmbatoss9D8",
+  authDomain: "portfolio-7825f.firebaseapp.com",
+  databaseURL:
+    "https://portfolio-7825f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "portfolio-7825f",
+  storageBucket: "portfolio-7825f.appspot.com",
+  messagingSenderId: "647004811939",
+  appId: "1:647004811939:web:bf73a2b237b78ff4c84516",
+  measurementId: "G-QN06NG2H8B",
 });
 
 export default app;
